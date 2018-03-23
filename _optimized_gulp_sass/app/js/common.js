cc750a521729243760112bab05a7908ce2958384
eugenypop <eugenypop@gmail.com>
2018-03-24 00:41:33 +0200

    Initial commit

---
+++ _optimized_gulp_sass/app/js/common.js
@@ -0,0 +1,26 @@
+(function () {
+
+    var text = 'Be sure you put your feet';
+    var textTwo = 'in the right place';
+    var i = 0;
+
+    function typeText() {
+        i++;
+        if (i <= text.length)
+            document.querySelector(".text-print").innerHTML = text.substr(0, i);
+        else if (i > text.length)
+            showText();
+
+        setTimeout(typeText, 90);
+    }
+
+    typeText();
+
+    var showText = function () {
+        var textPrintSec = document.querySelector('.text-print-sec');
+        textPrintSec.classList.add('trans-txt');
+        textPrintSec.innerHTML = textTwo;
+
+    };
+
+})();
